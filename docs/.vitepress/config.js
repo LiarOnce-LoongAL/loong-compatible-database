135f3a503e467b73ebd6ec3d77c2fa4848bc3cc4
LiarOnce <liaronce@hotmail.com>
2024-07-03 14:19:30 +0800

    config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,28 +1,69 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+import {
+  demoblockPlugin,
+  demoblockVitePlugin,
+} from "vitepress-theme-demoblock";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "Examples",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+  markdown: {
+    config: (md) => {
+      md.use(demoblockPlugin);
+    },
+  },
+  vite: {
+    plugins: [
+      demoblockVitePlugin(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+  demoblock: {
+    root: {
+      "view-source": "View source",
+      "hide-source": "Hide source",
+      "edit-in-editor": "Edit in Playground",
+      "edit-on-github": "Edit on GitHub",
+      "copy-code": "Copy code",
+      "copy-success": "Copy success",
+      "copy-error": "Copy error",
+    },
+    zh: {
+      "view-source": "查看源代码",
+      "hide-source": "隐藏源代码",
+      "edit-in-editor": "在 Playground 中编辑",
+      "edit-on-github": "在 Github 中编辑",
+      "copy-code": "复制代码",
+      "copy-success": "复制成功",
+      "copy-error": "复制失败",
+    },
+  },
+});
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,14 @@
+import DefaultTheme from "vitepress/theme";
+import "vitepress-theme-demoblock/dist/theme/styles/index.css";
+import { useComponents } from "./useComponents";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+export default {
+    ...DefaultTheme,
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+        useComponents(ctx.app);
+        ctx.app.use(ElementPlus, { locale: cn })
+    },
+};
