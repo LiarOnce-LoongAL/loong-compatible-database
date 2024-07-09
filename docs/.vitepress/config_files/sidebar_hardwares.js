0149ecdf7d724aeb73407b124b4130e178575f9d
LiarOnce <liaronce@hotmail.com>
2024-07-09 23:25:40 +0800

    Import data

--- docs/.vitepress/config_files/sidebar_hardwares.js
+++ docs/.vitepress/config_files/sidebar_hardwares.js
@@ -1,6 +1,7 @@
 const sidebar_hardwares = [
     { text: '首页', link: '/hardwares/index' },
-    { text: 'AMD 显卡 (AMDGPU)', link: '/hardwares/amdgpu' }
+    { text: 'AMD 显卡 (AMDGPU)', link: '/hardwares/amdgpu' },
+    { text: 'Intel 无线网卡 (iwlwifi)', link: '/hardwares/iwlwifi' }
 ]
 
 export { sidebar_hardwares }
