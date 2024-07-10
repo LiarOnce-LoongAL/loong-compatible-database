261cbbf271874ae10ffb076c48492872109373bb
LiarOnce <liaronce@hotmail.com>
2024-07-10 17:53:24 +0800

    Fix dark mode and link issue,

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,7 @@ import DefaultTheme from "vitepress/theme";
 import './custom.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/base.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 export default {
     ...DefaultTheme,
