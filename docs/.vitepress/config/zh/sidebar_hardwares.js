fb2813626abc1cd213e2ad1bc310a8521287f59f
LiarOnce <liaronce@hotmail.com>
2024-07-18 14:50:29 +0800

    Change nvidia info.

--- docs/.vitepress/config/zh/sidebar_hardwares.js
+++ docs/.vitepress/config/zh/sidebar_hardwares.js
@@ -1,6 +1,7 @@
 const sidebar_hardwares = [
     { text: '首页', link: '/hardwares/index' },
     { text: 'AMD 显卡 (AMDGPU)', link: '/hardwares/amdgpu' },
+    { text: 'NVIDIA 显卡', link: '/hardwares/nvidia' },
     { text: 'Intel 无线网卡 (iwlwifi)', link: '/hardwares/iwlwifi' }
 ]
 
