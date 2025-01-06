8786bb31261837614e2d1e6d0ece721a95dc2124
LiarOnce <liaronce@hotmail.com>
2025-01-06 17:07:03 +0800

    Add sidebar

--- docs/.vitepress/config/en/sidebar_hardwares.js
+++ docs/.vitepress/config/en/sidebar_hardwares.js
@@ -1,7 +1,9 @@
 const sidebar_hardwares = [
     { text: 'Home', link: '/en/hardwares/index' },
     { text: 'AMD Graphics (AMDGPU)', link: '/en/hardwares/amdgpu' },
-    { text: 'Intel Wireless (iwlwifi)', link: '/en/hardwares/iwlwifi' }
+    { text: 'NVIDIA Graphics', link: '/hardwares/nvidia' },
+    { text: 'Intel Wireless (iwlwifi)', link: '/en/hardwares/iwlwifi' },
+    { text: 'Moore Threads Graphics', link: '/hardwares/mthreads' }
 ]
 
 export { sidebar_hardwares }
--- docs/.vitepress/config/zh/sidebar_hardwares.js
+++ docs/.vitepress/config/zh/sidebar_hardwares.js
@@ -2,7 +2,8 @@ const sidebar_hardwares = [
     { text: '首页', link: '/hardwares/index' },
     { text: 'AMD 显卡 (AMDGPU)', link: '/hardwares/amdgpu' },
     { text: 'NVIDIA 显卡', link: '/hardwares/nvidia' },
-    { text: 'Intel 无线网卡 (iwlwifi)', link: '/hardwares/iwlwifi' }
+    { text: 'Intel 无线网卡 (iwlwifi)', link: '/hardwares/iwlwifi' },
+    { text: '摩尔线程显卡', link: '/hardwares/mthreads' }
 ]
 
 export { sidebar_hardwares }
