2e118ea91cac83ebbd6d4d7dd9ed3a331da9c720
LiarOnce <liaronce@hotmail.com>
2025-01-31 13:36:08 +0800

    sort list code look fine

--- docs/components/hardwares.vue
+++ docs/components/hardwares.vue
@@ -1,17 +1,28 @@
 <template>
-    <el-button @click="clearFilter">{{ $t('components.clear_filter') }}</el-button>
-    <el-table ref="tableRef" row-key="model" 
-        :data="tableData" border
+    <el-button @click="clearFilter">{{ $t("components.clear_filter") }}</el-button>
+    <el-table
+        ref="tableRef"
+        row-key="model"
+        :data="tableData"
+        border
         :default-sort="{ prop: 'brand', order: 'ascending' }"
     >
         <el-table-column prop="model" :label="$t('components.model')" width="300" />
-        <el-table-column prop="brand" :label="$t('components.brand')" column-key="brand" width="150" sortable 
-            :filters="filter_data[current_lang].filters_hardware_brand.sort(sortValue)" 
-            :filter-method="filterBrand" 
+        <el-table-column
+            prop="brand"
+            :label="$t('components.brand')"
+            column-key="brand"
+            width="150"
+            sortable
+            :filters="filter_data[current_lang].filters_hardware_brand.sort(sortValue)"
+            :filter-method="filterBrand"
         />
-        <el-table-column prop="type" :label="$t('components.type')" width="200" 
-            :filters="filter_data[current_lang].filters_hardware_type" 
-            :filter-method="filterType" 
+        <el-table-column
+            prop="type"
+            :label="$t('components.type')"
+            width="200"
+            :filters="filter_data[current_lang].filters_hardware_type"
+            :filter-method="filterType"
             filter-placement="bottom-end"
         >
             <template #default="scope">
@@ -20,17 +31,20 @@
                 </span>
             </template>
         </el-table-column>
-        <el-table-column prop="status" :label="$t('components.status')" width="170" 
-            :filters="filter_data[current_lang].filters_status" 
+        <el-table-column
+            prop="status"
+            :label="$t('components.status')"
+            width="170"
+            :filters="filter_data[current_lang].filters_status"
             :filter-method="filterStatus"
             filter-placement="bottom-end"
         >
             <template #default="scope">
-                <el-tag v-if="scope.row.status == 0" type="info">{{ $t('status.unknown') }}</el-tag>
-                <el-tag v-if="scope.row.status == 1" type="success">{{ $t('status.compatible') }}</el-tag>
-                <el-tag v-if="scope.row.status == 2" type="warning">{{ $t('status.partial') }}</el-tag>
-                <el-tag v-if="scope.row.status == 3">{{ $t('status.new_world') }}</el-tag>
-                <el-tag v-if="scope.row.status == -1" type="danger">{{ $t('status.unsupported') }}</el-tag>
+                <el-tag v-if="scope.row.status == 0" type="info">{{ $t("status.unknown") }}</el-tag>
+                <el-tag v-if="scope.row.status == 1" type="success">{{ $t("status.compatible") }}</el-tag>
+                <el-tag v-if="scope.row.status == 2" type="warning">{{ $t("status.partial") }}</el-tag>
+                <el-tag v-if="scope.row.status == 3">{{ $t("status.new_world") }}</el-tag>
+                <el-tag v-if="scope.row.status == -1" type="danger">{{ $t("status.unsupported") }}</el-tag>
             </template>
         </el-table-column>
         <el-table-column prop="notes" :label="$t('components.notes')" min-width="200">
@@ -41,58 +55,71 @@
         </el-table-column>
         <el-table-column prop="link" :label="$t('components.link')" width="60">
             <template #default="scope">
-                <span v-if="scope.row.link"><a :href="scope.row.link">{{ $t('components.doc_link') }}</a></span>
+                <span v-if="scope.row.link"
+                    ><a :href="scope.row.link">{{ $t("components.doc_link") }}</a></span
+                >
             </template>
         </el-table-column>
     </el-table>
 </template>
 
 <script setup>
-import { nextTick, ref, getCurrentInstance, onMounted } from 'vue'
-const { proxy } = getCurrentInstance()
-import { useI18n } from 'vue-i18n'
-const { locale } = useI18n()
+    import { nextTick, ref, getCurrentInstance, onMounted } from "vue";
+    const { proxy } = getCurrentInstance();
+    import { useI18n } from "vue-i18n";
+    const { locale } = useI18n();
 
-import databaseJson from '../data/datas.min.json'
-import filter_data from '../data/locales.min.json'
-const current_lang = document.documentElement.lang
+    import databaseJson from "../data/datas.min.json";
+    import filter_data from "../data/locales.min.json";
+    const current_lang = document.documentElement.lang;
 
-// let filtersJson
+    // let filtersJson
 
-const tableRef = ref()
+    const tableRef = ref();
 
-const clearFilter = () => {
-    tableRef.value.clearFilter()
-}
+    const clearFilter = () => {
+        tableRef.value.clearFilter();
+    };
 
-const filterBrand = (value, row) => {
-    return row.brand === value
-}
+    const filterBrand = (value, row) => {
+        return row.brand === value;
+    };
 
-const filterType = (value, row) => {
-    return row.type === value
-}
+    const filterType = (value, row) => {
+        return row.type === value;
+    };
 
-const filterStatus = (value, row) => {
-    return row.status === value
-}
+    const filterStatus = (value, row) => {
+        return row.status === value;
+    };
 
-const sortValue = (a, b) => {
-    // Convert upper case to sort list
-   return (a.value.toUpperCase() < b.value.toUpperCase()) ? -1 : (a.value.toUpperCase() > b.value.toUpperCase()) ? 1 : 0;
-}
+    const sortValue = (a, b) => {
+        // Convert upper case to sort list
+        const upperA = a.value.toUpperCase();
+        const upperB = b.value.toUpperCase();
 
-const tableData = databaseJson.hardwares
+        let result;
+        if (upperA < upperB) {
+            result = -1;
+        } else if (upperA > upperB) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        return result;
+    };
 
-nextTick(() => {
-    // Used for execute switch language after insert "lang" in <html>
-    if (typeof window !== "undefined") {
-        // Determine if the "window" exists to allow Vite to build smoothly
-        // There's black magic on the frontend, too!!!
-        localStorage.setItem('lang', document.documentElement.lang)
-        locale.value = localStorage.getItem('lang')
-        proxy.$forceUpdate()
-        // console.log(proxy)
-    }
-})
+    const tableData = databaseJson.hardwares;
+
+    nextTick(() => {
+        // Used for execute switch language after insert "lang" in <html>
+        if (typeof window !== "undefined") {
+            // Determine if the "window" exists to allow Vite to build smoothly
+            // There's black magic on the frontend, too!!!
+            localStorage.setItem("lang", document.documentElement.lang);
+            locale.value = localStorage.getItem("lang");
+            proxy.$forceUpdate();
+            // console.log(proxy)
+        }
+    });
 </script>
