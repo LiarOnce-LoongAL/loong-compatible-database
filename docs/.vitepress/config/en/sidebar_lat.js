ccdd33590fc03da4eca4e7b9327c78966e8ebf27
resbi <resbi@redstone.link>
2025-03-09 19:56:10 +0800

    Add key for Wolfram documents

--- docs/.vitepress/config/en/sidebar_lat.js
+++ docs/.vitepress/config/en/sidebar_lat.js
@@ -1,6 +1,7 @@
 const sidebar_lat = [
     { text: 'Home', link: '/en/lat/index' },
-    { text: 'SolidWorks', link: '/en/lat/s/solidworks' }
+    { text: 'SolidWorks', link: '/en/lat/s/solidworks' },
+    { text: 'Wolfram', link: '/en/lat/w/wolfram' }
 ]
 
 export { sidebar_lat }
