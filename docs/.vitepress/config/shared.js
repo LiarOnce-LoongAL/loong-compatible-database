cf06b5c80db98583010ba6326a2acbefe0f8eea4
LiarOnce <liaronce@hotmail.com>
2025-04-09 23:35:07 +0800

    Add ICP number, the website is loong123.cn now!

--- docs/.vitepress/config/shared.js
+++ docs/.vitepress/config/shared.js
@@ -12,7 +12,7 @@ export const shared = defineConfig({
     i18nRouting: true,
     themeConfig: {
         footer: {
-            message: 'CC BY-SA 4.0 International',
+            message: 'CC BY-SA 4.0 International <a href="https://beian.miit.gov.cn" target="_blank">鄂ICP备2022017735号-10</a>',
             copyright: 'Copyright © 2023-2025 <a href="https://github.com/LiarOnce" target="_blank">LiarOnce</a>'
         },
         search: {
